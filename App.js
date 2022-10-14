335eacee2a8f1a8f7eb4a864a932efceedb13420
Cem Mandraci <cemmandraci@hotmail.com>
2022-10-14 11:41:40 +0300

    #Verify weather api

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from "expo-Location";
+import DateTime from './components/datetime';
+import WeatherScroll from './components/wheatherScroll';
+import FutureForeCast from './components/futureforecast';
+
+const API_KEY = "b9207d97c5ec736583ce5c67cdbc47d9"
+const img = require("./assets/icon.png");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionAsync();
+      if ( status !== "granted"){
+        fetchDataFromApi("40.7128","-74.0060")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+    const fetchDataFromApi = (latitude, longitude) => {
+      if(latitude && longitude){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`).then(res => res.json()).then(data => {
+          setData(data)
+        })
+      }
+    }
+
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={assets} style={styles.image}>
+          <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+          <WeatherScroll weatherData={data.daily}/>
+        </ImageBackground>
+      </View>
+    );
 }
 
-const styles = StyleSheet.create({
+
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  }
 });
