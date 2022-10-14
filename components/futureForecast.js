42e5fceac79e4b8fd3c596ae49c509b49cc21f19
Cem Mandraci <cemmandraci@hotmail.com>
2022-10-14 10:37:40 +0300

    #futureForeCast styling

--- components/futureForecast.js
+++ components/futureForecast.js
@@ -34,3 +34,39 @@ const FutureForeCastItem = ({forecastItem}) => {
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    image: {
+        width: 100,
+        height: 100,
+    },
+    FutureForeCastItemContainer: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#00000033",
+        borderRadius: 10,
+        borderColor: "#eee",
+        borderWidth: 1,
+        padding: 20,
+        marginLeft: 10
+    },
+    day: {
+        fontSize: 20,
+        color: "white",
+        backgroundColor: "#3c3c44",
+        padding: 10,
+        textAlign: "center",
+        borderRadius: 50,
+        fontWeight: "200",
+        marginBottom: 15
+    },
+    temp: {
+        fontSize: 14,
+        color: "white",
+        fontWeight: "100",
+        textAlign: "center"
+    }
+});
+
+export default FutureForeCast;
